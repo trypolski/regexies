a1d3780aa495c060ec9737770ff5b840f8031720
Oleksandr Trypolskyi <a.trypolski@gmail.com>
2023-02-14 12:59:36 -0600

    TASK-191 Added GitHub pages and webpack config for documentation website

---
+++ config/constants.js
@@ -0,0 +1,8 @@
+// Entries names
+exports.HOME_PAGE_NAME = 'home';
+exports.PUBLIC_FILE_NAME = 'public';
+exports.REGEXIES_NAME = 'regexies';
+
+// Website pages
+exports.PAGES_FOLDER_PATH = './views/pages/';
+exports.CUSTOM_REGEX = new RegExp('.\/src\/pages\/(.*)\/main.js');
